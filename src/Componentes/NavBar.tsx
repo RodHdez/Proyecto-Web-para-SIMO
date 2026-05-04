042e24d0559749af20b7ae7a6d7bc42af50db4c1
RodHdez <mrodrigoh@yahoo.com>
2026-05-03 18:28:59 -0700

    Barra de Navegacion con Animacion
    Barra de navegacion ajustada al mockup con animacion

--- src/Componentes/NavBar.tsx
+++ src/Componentes/NavBar.tsx
@@ -1,13 +1,23 @@
 import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+import logo from "../assets/Logo_SIMO.png";
 
 export default function Navbar(){
     return(
-        <nav>
+        <nav className="navbar">
+            <div className="navbar-lado-logo">
+            <img src={logo} alt="logo" className="navbar-logo" />
+            </div>
+            <svg className="navbar-diagonal" viewBox="0 0 100 100" preserveAspectRatio="none">
+                <polygon points="0,0 0,100 35,100" fill="white"/>
+            </svg>
+            <div className="navbar-lado-links">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/Conocenos">Conocenos</NavLink>
             <NavLink to="/Socios">Socios</NavLink>
             <NavLink to="/Corporativa">Corporativa</NavLink>
             <NavLink to="/Contacto">Contacto</NavLink>
+            </div>
         </nav>
     )
 }
